4b23f4d73342a8754b5e834af1f30e0c0633fb90
Daniel <daniel.patek@email.cz>
2023-06-15 19:37:32 +0200

    added back user but as client component request

--- app/NavbarUser.tsx
+++ app/NavbarUser.tsx
@@ -1,13 +1,15 @@
-import { authOptions } from "@/lib/authOptions";
-import { getServerSession } from "next-auth";
+"use client";
 import React from "react";
 import ProfileIconButton from "./ProfileIconButton";
 import LoginButton from "./LoginButton";
+import { useSession } from "next-auth/react";
 
-async function NavbarUser() {
-    const session = await getServerSession(authOptions);
+function NavbarUser() {
+    const { data: session, status } = useSession();
 
-    return <>{session ? <ProfileIconButton user={session.user} /> : <LoginButton />}</>;
+    if (status === "loading") return <div className="w-[48px] h-[48px]"></div>;
+
+    return <>{status === "authenticated" ? <ProfileIconButton user={session.user} /> : <LoginButton />}</>;
 }
 
 export default NavbarUser;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import "./globals.css";
 import ThemeChanger from "./ThemeChanger";
 import NavbarUser from "./NavbarUser";
+import { NextAuthProvider } from "./providers";
 
 export const revalidate = 3600;
 
@@ -10,56 +11,58 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <html lang="cs" className="font-sans">
             <head />
             <body>
-                <div className="navbar bg-base-100 border-b border-base-300 py-3 px-9">
-                    <div className="navbar-start">
-                        <div className="dropdown">
-                            <label tabIndex={0} className="btn btn-ghost md:hidden">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-                                </svg>
-                            </label>
-                            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                <NextAuthProvider>
+                    <div className="navbar bg-base-100 border-b border-base-300 py-3 px-9">
+                        <div className="navbar-start">
+                            <div className="dropdown">
+                                <label tabIndex={0} className="btn btn-ghost md:hidden">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                                    </svg>
+                                </label>
+                                <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                                    <li>
+                                        <Link href="/rozhledny">Rozhledny</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/mapa">Mapa</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/komunita">Komunita</Link>
+                                    </li>
+                                </ul>
+                            </div>
+                            <Link href="/" className="btn btn-ghost normal-case text-lg md:text-xl lg:text-3xl ml-[-0.5rem]">
+                                ROZHLEDNOVÝ SVĚT
+                            </Link>
+                        </div>
+                        <div className="navbar-center hidden md:flex">
+                            <ul className="menu menu-horizontal px-1">
                                 <li>
-                                    <Link href="/rozhledny">Rozhledny</Link>
+                                    <Link className="active:bg-secondary" href="/rozhledny">
+                                        Rozhledny
+                                    </Link>
                                 </li>
                                 <li>
-                                    <Link href="/mapa">Mapa</Link>
+                                    <Link className="active:bg-secondary" href="/mapa">
+                                        Mapa
+                                    </Link>
                                 </li>
                                 <li>
-                                    <Link href="/komunita">Komunita</Link>
+                                    <Link className="active:bg-secondary" href="/komunita">
+                                        Komunita
+                                    </Link>
                                 </li>
                             </ul>
                         </div>
-                        <Link href="/" className="btn btn-ghost normal-case text-lg md:text-xl lg:text-3xl ml-[-0.5rem]">
-                            ROZHLEDNOVÝ SVĚT
-                        </Link>
-                    </div>
-                    <div className="navbar-center hidden md:flex">
-                        <ul className="menu menu-horizontal px-1">
-                            <li>
-                                <Link className="active:bg-secondary" href="/rozhledny">
-                                    Rozhledny
-                                </Link>
-                            </li>
-                            <li>
-                                <Link className="active:bg-secondary" href="/mapa">
-                                    Mapa
-                                </Link>
-                            </li>
-                            <li>
-                                <Link className="active:bg-secondary" href="/komunita">
-                                    Komunita
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="navbar-end flex gap-3">
-                        <ThemeChanger />
+                        <div className="navbar-end flex gap-3">
+                            <ThemeChanger />
 
-                        {/* <NavbarUser /> */}
+                            <NavbarUser />
+                        </div>
                     </div>
-                </div>
-                {children}
+                    {children}
+                </NextAuthProvider>
             </body>
         </html>
     );
