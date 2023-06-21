1850df31ae1fcabcf3bee38ffce66d90e8a523ba
dowl <xpatek08@stud.fit.vutbr.cz>
2023-06-21 19:41:14 +0200

    history text padding fix

--- app/[type]/[nameID]/HistoryText.tsx
+++ app/[type]/[nameID]/HistoryText.tsx
@@ -6,8 +6,8 @@ type PageProps = {
 
 function HistoryText({ text }: PageProps) {
     return (
-        <div tabIndex={0} className="collapse collapse-arrow w-full shadow-lg rounded-box border border-secondary-focus px-4 py-4">
-            <input type="checkbox" className="peer" />
+        <div tabIndex={0} className="collapse collapse-arrow w-full shadow-lg rounded-box border border-secondary-focus ">
+            <input type="checkbox" className="peer px-10 py-10" />
             <div className="collapse-title card-title text-xl">Podrobný popis a historie rozhledny</div>
             <div className="collapse-content text-lg text-justify">
                 <p>{text}</p>
