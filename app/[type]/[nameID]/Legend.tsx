1a8ea86921ba3f2d497b91e1bc9e7311ddaabebb
Daniel <daniel.patek@email.cz>
2023-07-14 11:57:54 +0200

    fixed legend generation

--- app/[type]/[nameID]/Legend.tsx
+++ app/[type]/[nameID]/Legend.tsx
@@ -1,19 +1,29 @@
 import { Tower } from "@/typings";
+import { OpeningHoursForbiddenType, OpeningHoursType } from "@/utils/constants";
 import React from "react";
 
 function capitalizeFirstLetter(string: string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+const generateHeading = (openingHours: OpeningHours): string => {
+    if (openingHours.type === OpeningHoursType.Unknown) return " má neznámou otevírací dobu.";
+    if (openingHours.type === OpeningHoursType.NonStop) return ` je volně přístupná.`;
+    if (openingHours.type === OpeningHoursType.Forbidden) {
+        if (openingHours.forbidden_type === OpeningHoursForbiddenType.Reconstruction) return ` je právě v rekonstrukci.`;
+        if (openingHours.forbidden_type === OpeningHoursForbiddenType.Temporary) return ` je dočasně uzavřena.`;
+        return ` je označena jako zaniklá.`;
+    }
+    if (openingHours.type === OpeningHoursType.Occasionally) return ` je přístupná pouze příležitostně.`;
+    if (openingHours.months?.length === 0) return ` je otevřena celoročně (viz níže).`;
+    return " otevírací doba je uvedena níže.";
+};
+
 function Legend({ tower }: { tower: Tower }) {
     return (
         <legend>{`${capitalizeFirstLetter(tower.type)} je vysoká ${tower.height === -1 ? "neznámo" : tower.height} metrů a vede na ni ${
             tower.stairs === -1 ? "neznámo" : "přesně " + tower.stairs
-        } schodů. Nachází se v nadmořské výšce ${tower.elevation} metrů a ${tower.openingHours === "volně přístupná" ? "je volně přístupná" : ""}${
-            tower.openingHours === "dlouhodobě uzavřená" ? "je bohužel dlouhodobě uzavřená" : ""
-        }${
-            "dlouhodobě uzavřená" !== tower.openingHours && tower.openingHours !== "volně přístupná" ? "její otevírací doba není známa" : ""
-        }.`}</legend>
+        } schodů. Nachází se v nadmořské výšce ${tower.elevation} metrů a ${generateHeading(tower.openingHours)}`}</legend>
     );
 }
 
