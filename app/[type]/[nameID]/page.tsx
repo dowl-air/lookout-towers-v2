2e8ce65b1acccf786cb2442233be249e2e12f1d2
dowl <xpatek08@stud.fit.vutbr.cz>
2023-08-08 15:35:18 +0200

    history text tile updated

--- app/[type]/[nameID]/page.tsx
+++ app/[type]/[nameID]/page.tsx
@@ -79,7 +79,7 @@ async function TowerPage({ params: { type, nameID } }: PageProps) {
             </div>
             <div
                 id={"bottom"}
-                className={"flex flex-col gap-12 items-center justify-center self-center mb-6 mx-1 sm:mx-3 flex-1 max-w-screen-xl w-full"}
+                className={"flex flex-col gap-6 items-center justify-center self-center mb-6 mx-1 sm:mx-3 flex-1 max-w-screen-xl w-full"}
             >
                 <div className={"flex flex-wrap gap-3 w-full items-center justify-center"}>
                     <OpeningHours tower={tower}>
