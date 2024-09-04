28a1cb8d921eb4dfd9723b8fb91bc55d3749ac96
Daniel Pátek <daniel.patek@email.cz>
2024-09-04 20:23:49 +0200

    fix: rating scroll

--- components/tower/top/RatingTop.tsx
+++ components/tower/top/RatingTop.tsx
@@ -2,16 +2,16 @@
 import ThemedRating from "@/components/shared/ThemedRating";
 
 const RatingTop = ({ count, average }: { count: number; average: number }) => {
-    const scroll_ = () => document?.querySelector("#rating_box")?.scrollIntoView({ behavior: "smooth", block: "center" });
+    const scroll_ = () => document?.querySelector("#tower-rating-form")?.scrollIntoView({ behavior: "smooth", block: "center" });
     return (
-        <div>
-            <div onClick={scroll_}>
+        <>
+            <div onClick={scroll_} className="mt-4">
                 <ThemedRating value={average} size={45} className="lg:mt-9 cursor-pointer" />
             </div>
             <p className="text-sm mt-0 pl-1 cursor-pointer" onClick={() => scroll()}>
                 {count ? `Hodnoceno ${count}x` : "Zatím nikdo nehodnotil."}
             </p>
-        </div>
+        </>
     );
 };
 
