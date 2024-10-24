ced520968bf8ab9c75b4c7488f88cb6c8f97abfa
dowl <daniel.patek@email.cz>
2024-10-24 23:45:56 +0200

    chore: remove dead code

--- utils/getThemeColors.ts
+++
@@ -1,6 +0,0 @@
-const colorNames = require("daisyui/src/theming/themes")
-
-export const getThemeColors = (theme: string | undefined) : any => {
-    const name = `[data-theme=${theme || "light"}]`;
-    return colorNames[name];
-}
