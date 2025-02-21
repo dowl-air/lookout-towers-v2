6bd249b56f65017813fa3b172aff33226c14ebde
Daniel Patek <daniel.patek@firma.seznam.cz>
2025-02-21 14:55:36 +0100

    feat: add authentication check before editing parameters

--- components/tower/tiles/parameters/dialog/EditButton.tsx
+++ components/tower/tiles/parameters/dialog/EditButton.tsx
@@ -1,14 +1,22 @@
 "use client";
 
+import { checkAuth } from "@/actions/checkAuth";
 import { showModalWithoutFocus } from "@/utils/showModal";
+import { useRouter } from "next/navigation";
 
 const EditButton = () => {
+    const router = useRouter();
+
     return (
         <button
             className="btn btn-warning"
-            onClick={() => {
-                (document.getElementById("parameters-modal") as HTMLDialogElement).close();
-                showModalWithoutFocus("edit_parameters");
+            onClick={async () => {
+                if ((await checkAuth()) !== null) {
+                    (document.getElementById("parameters-modal") as HTMLDialogElement).close();
+                    showModalWithoutFocus("edit_parameters");
+                } else {
+                    return router.push("/signin");
+                }
             }}
         >
             Navrhnout úpravu
