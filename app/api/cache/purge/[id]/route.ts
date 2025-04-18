597145312103349f2b3f858a32ea6cc9d37ce2b6
Daniel Patek <daniel.patek@firma.seznam.cz>
2025-04-18 11:03:17 +0200

    fix: await response

--- app/api/cache/purge/[id]/route.ts
+++ app/api/cache/purge/[id]/route.ts
@@ -3,9 +3,9 @@ import { revalidateTowerByIDOrNameID } from "@/actions/cache/purge.tower.action"
 export async function POST(request: Request, { params }: { params: Promise<{ id: string }> }) {
     const id = (await params).id;
 
-    const idResp = revalidateTowerByIDOrNameID(id);
+    const idResp = await revalidateTowerByIDOrNameID(id);
 
-    return new Response(JSON.stringify(idResp), {
+    return new Response(JSON.stringify({ result: idResp }), {
         status: 201,
         headers: { "Content-Type": "application/json" },
     });
