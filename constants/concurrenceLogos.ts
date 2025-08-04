fd74a326a44941788045bb0943264978f3621dfd
Daniel Patek <daniel.patek@firma.seznam.cz>
2025-08-04 21:53:29 +0200

    fix: mapy.com pic

--- constants/concurrenceLogos.ts
+++ constants/concurrenceLogos.ts
@@ -1,6 +1,7 @@
 export const CONCURRENCE_LOGOS: Record<string, string> = {
     "rozhledny.webzdarma.cz": "https://rozhledny.webzdarma.cz/logo.jpg",
     "mapy.cz": "/img/loga/mapy-cz-symbol-rgb.svg",
+    "mapy.com": "/img/loga/mapy-cz-symbol-rgb.svg",
     "wikipedia.org": "/img/loga/Wikipedia_W.svg",
     "wikimedia.org": "/img/loga/Wikimedia-logo.svg",
     "cs.wikipedia.org": "/img/loga/Wikipedia_W.svg",
