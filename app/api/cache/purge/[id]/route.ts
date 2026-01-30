f1203de94113c5b3bf3b49666f32b3017abbae90
Daniel Patek <daniel.patek@firma.seznam.cz>
2026-01-30 09:34:21 +0100

    fix: cache invalidation POST

--- app/api/cache/purge/[id]/route.ts
+++ app/api/cache/purge/[id]/route.ts
@@ -1,11 +1,29 @@
-import { revalidateTowerByIDOrNameID } from "@/actions/cache/purge.tower.action";
+import { revalidateTag } from "next/cache";
+
+import { getTowerByID, getTowerObjectByNameID } from "@/actions/towers/towers.action";
+import { CacheTag, getCacheTagSpecific } from "@/utils/cacheTags";
 
 export async function POST(request: Request, { params }: { params: Promise<{ id: string }> }) {
-    const id = (await params).id;
+    const tower_id = (await params).id;
 
-    const idResp = await revalidateTowerByIDOrNameID(id);
+    if (!tower_id) return null;
+    let tower = await getTowerByID(tower_id);
+    if (!tower) {
+        tower = await getTowerObjectByNameID(tower_id);
+        if (!tower) return "Tower not found";
+    }
 
-    return new Response(JSON.stringify({ result: idResp }), {
+    revalidateTag(CacheTag.Towers, { expire: 0 });
+    revalidateTag(CacheTag.TowersCount, { expire: 0 });
+    revalidateTag(CacheTag.LastChangeDate, { expire: 0 });
+    revalidateTag(getCacheTagSpecific(CacheTag.Tower, tower.id), { expire: 0 });
+    revalidateTag(getCacheTagSpecific(CacheTag.Tower, tower.nameID), { expire: 0 });
+    revalidateTag(getCacheTagSpecific(CacheTag.TowerGallery, tower.id), { expire: 0 });
+    revalidateTag(getCacheTagSpecific(CacheTag.TowerRatingAndCount, tower.id), { expire: 0 });
+    revalidateTag(getCacheTagSpecific(CacheTag.TowerVisitsCount, tower.id), { expire: 0 });
+    revalidateTag(getCacheTagSpecific(CacheTag.TowerPhotos, tower.id), { expire: 0 });
+
+    return new Response(JSON.stringify({ result: tower.id }), {
         status: 201,
         headers: { "Content-Type": "application/json" },
     });
